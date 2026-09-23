6067eaa985286525444039c1c651c3c8b3a6d3ae
ochoa07 <och23008@byui.edu>
2026-09-23 13:12:12 -0600

    Set up Node and Express practice server

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const app = express();
+const name = process.env.NAME;
+
+app.get('/', (req, res) => {
+  res.send(`Welcome, ${name}!`);
+});
+
+const PORT = 3000;
+
+app.get('/new-route', (req, res) => {
+  res.send('This is a new route!');
+});
+app.listen(PORT, () => {
+  console.log(`Server is running on http://127.0.0.1:${PORT}`);
+});
