85190885ddecfb410530c7c87ca0b7aa253f424a
ochoa07 <och23008@byui.edu>
2026-09-23 21:25:37 -0600

    Convert pages to EJS templates

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import { fileURLToPath } from 'url';
 import path from 'path';
 
 const PORT = process.env.PORT || 3000;
+const NODE_ENV = process.env.NODE_ENV || 'production';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
