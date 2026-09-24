4d4f9f72aba3bb8e4c9d9ae0adfe789ff8d0b6a6
ochoa07 <och23008@byui.edu>
2026-09-23 21:22:11 -0600

    Convert pages to EJS templates

--- server.js
+++ server.js
@@ -9,17 +9,22 @@ const __dirname = path.dirname(__filename);
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'src/views'));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'src/views/home.html'));
+  const title = 'Welcome Home';
+  res.render('home', { title });
 });
 
 app.get('/about', (req, res) => {
-  res.sendFile(path.join(__dirname, 'src/views/about.html'));
+  const title = 'About Me';
+  res.render('about', { title });
 });
 
 app.get('/products', (req, res) => {
-  res.sendFile(path.join(__dirname, 'src/views/products.html'));
+  const title = 'Our Products';
+  res.render('products', { title });
 });
 
 app.listen(PORT, () => {
